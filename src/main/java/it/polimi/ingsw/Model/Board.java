0214a23e599dfcf8bc83c81f56b35e4700296c85
gioeleverze <gioele.verze@polimi.it>
2020-03-19 13:00:23 +0100

    Aggiornato Board e UML

--- src/main/java/it/polimi/ingsw/Model/Board.java
+++ src/main/java/it/polimi/ingsw/Model/Board.java
@@ -8,11 +8,13 @@ public class Board {
         int x;
         int y;
         for(y=0;y<5;y++)
-            for(x=0;x<5;x++)
+            for(x=0;x<5;x++) {
                 board[x][y]=new Box();
+            }
     }
 
     public void move(Worker w,int x,int y)
     {
+        board[x][y].setPawn(w);
     }
 }
