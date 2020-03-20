f16be4d1340f92521e3955e024fdfb7fcb4af755
Anacleto98 <simone.tome@polimi.it>
2020-03-20 11:47:45 +0100

    Aggiunti i valori interi nella enum

--- src/main/java/it/polimi/ingsw/Model/Block.java
+++ src/main/java/it/polimi/ingsw/Model/Block.java
@@ -1,10 +1,16 @@
 package it.polimi.ingsw.Model;
 
+
+
 public enum Block {
 
-    TERRAIN,
-    LONE,
-    LTWO,
-    LTHREE,
-    DOME;
+    TERRAIN(0),LONE(1),LTWO(2),LTHREE(3),DOME(4);
+
+    private int value;
+    private Block(int value){
+        this.value = value;
+    }
+    public int getValue() {
+        return value;
+    }
 }
