8733812c1a8da755af30e877fd5147d9f946df4b
Anacleto98 <simone.tome@polimi.it>
2020-03-22 10:09:01 +0100

    Aggiornati i test per defaultMove e defaultBuild correttamente: va implementato il controllo per la posizione iniziale dei worker e coerenza tra coordinate

--- src/test/java/it/polimi/ingsw/Model/DefaultBuildTest.java
+++ src/test/java/it/polimi/ingsw/Model/DefaultBuildTest.java
@@ -7,8 +7,8 @@ import static org.junit.Assert.*;
 public class DefaultBuildTest {
 
 
-    @Test (expected = RuntimeException.class)
-    public void build() {
+    @Test
+    public void rightBuild() {
         Board board = new Board();
         Game game = new Game("Marco","Giuseppe",board,true);
         Player player = new Player("Marco",game);
@@ -30,11 +30,35 @@ public class DefaultBuildTest {
         builder.build(x,y);
         actualLevel = board.getBox(x,y).getBlock().getValue();
         assertEquals(4,actualLevel);
+    }
+    @Test (expected = RuntimeException.class)
+    public void noBuildOverDome() {
+        Board board = new Board();
+        Game game = new Game("Marco","Giuseppe",board,true);
+        Player player = new Player("Marco",game);
+        DefaultBuild builder = new DefaultBuild(player);
+        int actualLevel;
+        int x = 0;
+        int y = 0;
+        builder.build(x,y);
+        builder.build(x,y);
+        builder.build(x,y);
         builder.build(x,y);
         actualLevel = board.getBox(x,y).getBlock().getValue();
         assertEquals(4,actualLevel);
-
-
-
+        builder.build(x,y);
+        actualLevel = board.getBox(x,y).getBlock().getValue();
     }
+//    @Test (expected = RuntimeException.class)
+//    public void noBuildOverWorker() {
+//        Board board = new Board();
+//        Game game = new Game("Marco","Giuseppe",board,true);
+//        Player player = new Player("Marco",game);
+//        DefaultBuild builder = new DefaultBuild(player);
+//        Worker worker = player.getWorker1();
+//        int actualLevel;
+//        board.placePawn(worker,0,0);
+//        builder.build(0,0);
+//    }
+
 }
