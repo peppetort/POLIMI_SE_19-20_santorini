514fddd462d2258804931f58ca21f1ac3bfd54a4
Anacleto98 <simone.tome@polimi.it>
2020-04-01 15:03:47 +0200

    Aggiunto il test del controller

---
+++ src/test/java/it/polimi/ingsw/Controller/ControllerTest.java
@@ -0,0 +1,88 @@
+package it.polimi.ingsw.Controller;
+
+
+import it.polimi.ingsw.Model.*;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+import static org.junit.Assert.*;
+
+public class ControllerTest {
+
+    @Test
+    public void DefaultTurnNoErr() {
+        Board board = new Board();
+        Game game = new Game("Marco","Giovanni",board,false);
+        Player player1 = new Player("Marco",game);
+        Player player2 = new Player("Giovanni",game);
+        Controller controller = new Controller(game);
+        board.placePawn(game.getPlayers().get(0).getWorker1(),0,0);
+        board.placePawn(game.getPlayers().get(1).getWorker1(),2,2);
+
+        //Player1 has to choose the cards
+
+        ArrayList<Card> deck = new ArrayList<>();
+        Card c1 = new Card(God.PAN);
+        Card c2 = new Card(God.DEMETER);
+        deck.add(c1);
+        deck.add(c2);
+        DeckChoice deckChoice = new DeckChoice(game.getPlayers().get(0),deck);
+
+        assertEquals("Marco",game.getPlayers().get(0).getUsername());
+
+        //The View notifies the controller which update itself with the message DeckChoice as a parameter
+        assertEquals(true,controller.getTurn().get(game.getPlayers().get(0)));
+        assertEquals(false,controller.getTurn().get(game.getPlayers().get(1)));
+        controller.update(deckChoice);
+        assertEquals(true,controller.getFirstTurn());
+        assertEquals(God.PAN,game.getCards().get(0).getName());
+        assertEquals(God.DEMETER,game.getCards().get(1).getName());
+        //The turn has been updated
+        assertEquals(false,controller.getTurn().get(game.getPlayers().get(0)));
+        assertEquals(true,controller.getTurn().get(game.getPlayers().get(1)));
+
+        //Player2 has to choose the card and he wants PAN
+        CardChoice cardChoice = new CardChoice(game.getPlayers().get(1),game.getCards().get(0));
+        controller.update(cardChoice);
+        assertEquals(God.PAN,game.getPlayers().get(1).getCard().getName());
+        assertEquals(God.DEMETER,game.getPlayers().get(0).getCard().getName());
+        assertEquals(true,controller.getTurn().get(game.getPlayers().get(0)));
+        assertEquals(false,controller.getTurn().get(game.getPlayers().get(1)));
+
+        //now the game has to start, player 1 can move 1 time and build one block
+        //p1S is the message of startTurn for player1 with the first of his workers
+        player1 = game.getPlayers().get(0);
+        player2 = game.getPlayers().get(1);
+        PlayerStart p1S = new PlayerStart(player1,player1.getWorker1());
+        controller.update(p1S);
+        assertEquals(true,player1.getTurn() instanceof DemeterTurn);
+        PlayerMove p1M = new PlayerMove(player1,0,1);
+        PlayerBuild p1B = new PlayerBuild(player1,0,2);
+        controller.update(p1M);
+        assertEquals(player1.getWorker1(),board.getBox(0,1).getPawn());
+        controller.update(p1B);
+        assertEquals(Block.LONE,board.getBox(0,2).getBlock());
+        PlayerBuild p2B = new PlayerBuild(player1,1,2);
+        controller.update(p2B);
+        assertEquals(Block.LONE,board.getBox(1,2).getBlock());
+        PlayerEnd p1e = new PlayerEnd(player1);
+        controller.update(p1e);
+
+        assertEquals(false,controller.getTurn().get(player1));
+        assertEquals(true,controller.getTurn().get(player2));
+
+        //turno del player2...
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
