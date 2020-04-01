863c05b8a5f3acf9a5a6b9afb4b6ee9693c5d6df
Anacleto98 <simone.tome@polimi.it>
2020-04-02 01:07:12 +0200

    Aggiornato ControllerTest sulla base degli aggiornamenti della class

--- src/test/java/it/polimi/ingsw/Controller/ControllerTest.java
+++ src/test/java/it/polimi/ingsw/Controller/ControllerTest.java
@@ -19,15 +19,18 @@ public class ControllerTest {
         Controller controller = new Controller(game);
         board.placePawn(game.getPlayers().get(0).getWorker1(),0,0);
         board.placePawn(game.getPlayers().get(1).getWorker1(),2,2);
-
-        //Player1 has to choose the cards
+        ArrayList<Player> players = new ArrayList<>();
+        players.add(player1);
+        players.add(player2);
+        InitializePlayersMessage iMessage = new InitializePlayersMessage(players);
+        controller.initializePlayers(iMessage);
 
         ArrayList<Card> deck = new ArrayList<>();
         Card c1 = new Card(God.PAN);
         Card c2 = new Card(God.DEMETER);
         deck.add(c1);
         deck.add(c2);
-        DeckChoice deckChoice = new DeckChoice(game.getPlayers().get(0),deck);
+        DeckChoice deckChoice = new DeckChoice(player1,deck);
 
         assertEquals("Marco",game.getPlayers().get(0).getUsername());
 
@@ -43,7 +46,7 @@ public class ControllerTest {
         assertEquals(true,controller.getTurn().get(game.getPlayers().get(1)));
 
         //Player2 has to choose the card and he wants PAN
-        CardChoice cardChoice = new CardChoice(game.getPlayers().get(1),game.getCards().get(0));
+        CardChoice cardChoice = new CardChoice(player2,game.getCards().get(0));
         controller.update(cardChoice);
         assertEquals(God.PAN,game.getPlayers().get(1).getCard().getName());
         assertEquals(God.DEMETER,game.getPlayers().get(0).getCard().getName());
@@ -52,15 +55,14 @@ public class ControllerTest {
 
         //now the game has to start, player 1 can move 1 time and build one block
         //p1S is the message of startTurn for player1 with the first of his workers
-        player1 = game.getPlayers().get(0);
-        player2 = game.getPlayers().get(1);
+
         PlayerStart p1S = new PlayerStart(player1,player1.getWorker1());
         controller.update(p1S);
-        assertEquals(true,player1.getTurn() instanceof DemeterTurn);
+        assertEquals(true,game.getPlayers().get(0).getTurn() instanceof DemeterTurn);
         PlayerMove p1M = new PlayerMove(player1,0,1);
         PlayerBuild p1B = new PlayerBuild(player1,0,2);
         controller.update(p1M);
-        assertEquals(player1.getWorker1(),board.getBox(0,1).getPawn());
+        assertEquals(game.getPlayers().get(0).getWorker1(),board.getBox(0,1).getPawn());
         controller.update(p1B);
         assertEquals(Block.LONE,board.getBox(0,2).getBlock());
         PlayerBuild p2B = new PlayerBuild(player1,1,2);
@@ -68,21 +70,41 @@ public class ControllerTest {
         assertEquals(Block.LONE,board.getBox(1,2).getBlock());
         PlayerEnd p1e = new PlayerEnd(player1);
         controller.update(p1e);
-
-        assertEquals(false,controller.getTurn().get(player1));
-        assertEquals(true,controller.getTurn().get(player2));
-
-        //turno del player2...
-
-
-
-
-
-
-
-
-
-
+        assertEquals(false,controller.getTurn().get(game.getPlayers().get(0)));
+        assertEquals(true,controller.getTurn().get(game.getPlayers().get(1)));
 
     }
+    @Test
+    public void testTurnInterrupt() {
+        Board board = new Board();
+        Game game = new Game("Marco","Giovanni",board,false);
+        Player player1 = new Player("Marco",game);
+        Player player2 = new Player("Giovanni",game);
+        Controller controller = new Controller(game);
+        board.placePawn(game.getPlayers().get(0).getWorker1(),0,0);
+        board.placePawn(game.getPlayers().get(1).getWorker1(),2,2);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 }
