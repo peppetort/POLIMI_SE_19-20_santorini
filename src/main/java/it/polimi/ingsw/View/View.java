61baf78d9a58a89ded7cc9d6745bc33709ca738b
Anacleto98 <simone.tome@polimi.it>
2020-05-09 00:45:53 +0200

    Now the remoteview doesn't receive a string (and split it to retrieve data for the action). It receive a message and the View handle that and notifies the Controller object.

--- src/main/java/it/polimi/ingsw/View/View.java
+++ src/main/java/it/polimi/ingsw/View/View.java
@@ -1,15 +1,12 @@
 package it.polimi.ingsw.View;
 
-
 import it.polimi.ingsw.Messages.*;
 import it.polimi.ingsw.Model.Player;
 import it.polimi.ingsw.Observer.Observable;
 import it.polimi.ingsw.Observer.Observer;
 
-import java.util.HashSet;
-import java.util.Set;
 
-public abstract class View extends Observable<Message> implements Observer<Message> {
+public abstract class View extends Observable<Message> implements Observer<Message>{
 
     Player player;
 
@@ -17,51 +14,57 @@ public abstract class View extends Observable<Message> implements Observer<Messa
         this.player = player;
     }
 
-    public void handleDeck(String card1, String card2, String card3) {
-        Set<String> cardsNames = new HashSet<>();
-        cardsNames.add(card1);
-        cardsNames.add(card2);
-        cardsNames.add(card3);
-        notify(new PlayerDeckMessage(player, cardsNames));
+    public void handleDeck(Message message) {
+        ((PlayerDeckMessage)message).setPlayer(player);
+        notify(message);
     }
 
-    public void handleDeck(String card1, String card2) {
-        Set<String> cardsNames = new HashSet<>();
-        cardsNames.add(card1);
-        cardsNames.add(card2);
-        notify(new PlayerDeckMessage(player, cardsNames));
+    public void handleCardChoice(Message message) {
+        ((PlayerCardChoiceMessage)message).setPlayer(player);
+        notify(message);
     }
 
-    public void handleCard(String card) {
-        notify(new PlayerCardChoiceMessage(player, card));
-    }
-
-    public void handleStart(int w) {
-        if (w == 1) {
-            notify(new PlayerStartMessage(player, player.getWorker1()));
-        } else if (w == 2) {
-            notify(new PlayerStartMessage(player, player.getWorker2()));
+    public void handleSelect(Message message){
+        ((PlayerSelectMessage)message).setPlayer(player);
+        switch(((PlayerSelectMessage)message).getNum()){
+            case 1:
+                ((PlayerSelectMessage)message).setWorker(player.getWorker1());
+                break;
+            case 2:
+                ((PlayerSelectMessage)message).setWorker(player.getWorker2());
+                break;
+                //TODO : devo lanciare un'eccezione in caso che io abbia selezionato piu di 3 player
         }
+        notify(message);
     }
 
-    public void handleMove(int row, int column) {
-        notify(new PlayerMoveMessage(player, row, column));
+    public void handleMove(Message message) {
+        ((PlayerMoveMessage)message).setPlayer(player);
+        notify(message);
     }
 
-    public void handleBuild(int row, int column) {
-        notify(new PlayerBuildMessage(player, row, column));
+    public void handleBuild(Message message) {
+        ((PlayerBuildMessage)message).setPlayer(player);
+        notify(message);
     }
 
-    public void handleBuildDome(int row, int column) {
-        notify(new PlayerBuildDomeMessage(player, row, column));
+    public void handleBuildDome(Message message) {
+        ((PlayerBuildDomeMessage)message).setPlayer(player);
+        notify(message);
     }
 
-    public void handlePlacing(int x1, int y1, int x2, int y2) {
-        notify(new PlayerPlacePawnsMessage(player, x1, y1, x2, y2));
+    public void handlePlacing(Message message) {
+        ((PlayerPlacePawnsMessage)message).setPlayer(player);
+        notify(message);
     }
-    public void handleUndo(){notify(new PlayerUndoMessage(player));}
-    public void handleEnd() {
-        notify(new PlayerEndMessage(player));
+    public void handleUndo(Message message){
+        ((PlayerUndoMessage)message).setPlayer(player);
+        notify(message);
+    }
+
+    public void handleEnd(Message message) {
+        ((PlayerEndMessage)message).setPlayer(player);
+        notify(message);
     }
 
 }
