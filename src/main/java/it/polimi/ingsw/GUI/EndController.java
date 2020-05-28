0f49e40462e3c3e08688976619c1ccec91aaa324
Giuseppe Tortorelli <10582962@polimi.it>
2020-05-28 14:57:05 +0200

    Creato schermata di end e schermata di win

---
+++ src/main/java/it/polimi/ingsw/GUI/EndController.java
@@ -0,0 +1,24 @@
+package it.polimi.ingsw.GUI;
+
+
+import javafx.animation.*;
+import javafx.fxml.Initializable;
+import javafx.scene.image.ImageView;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+
+public class EndController implements Initializable {
+
+	@Override
+	public void initialize(URL url, ResourceBundle resourceBundle) {
+
+	}
+
+	public void handleEnd(){
+		//TODO
+	}
+
+}
